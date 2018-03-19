fe96914f98aaf124c3edebaf7554e194491bf881
哘雅楽乃 <yusk5497hnd@gmail.com>
2018-03-19 18:19:41 +0900

    Add files via upload

---
+++ build/CMakeFiles/VitaShell_JP.vpk.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VitaShell_JP.vpk"
+  "VitaShell_JP.vpk_param.sfo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/VitaShell_JP.vpk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
