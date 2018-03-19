0bcba87da6b8b3857ec9e6798064e003912eb4ca
哘雅楽乃 <yusk5497hnd@gmail.com>
2018-03-19 18:41:38 +0900

    Add files via upload

---
+++ modules/usbdevice/build/CMakeFiles/usbdevice.skprx.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/usbdevice.skprx"
+  "usbdevice.velf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/usbdevice.skprx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
