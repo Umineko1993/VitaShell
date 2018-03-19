0237cec4ff8063c3d438aad22144d819f842ea35
哘雅楽乃 <yusk5497hnd@gmail.com>
2018-03-19 18:27:21 +0900

    Add files via upload

---
+++ modules/kernel/build/CMakeFiles/kernel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kernel.dir/main.c.obj"
+  "kernel.pdb"
+  "kernel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kernel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
