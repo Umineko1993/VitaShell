f88416daad8b18254ce623e694d8a7a97ecbf399
哘雅楽乃 <yusk5497hnd@gmail.com>
2018-03-19 18:36:11 +0900

    Add files via upload

---
+++ modules/patch/build/CMakeFiles/patch.skprx.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/patch.skprx"
+  "patch.velf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/patch.skprx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
