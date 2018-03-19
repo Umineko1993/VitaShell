5a57bc925e456c64ca5cb366a6fad99b110bcc9e
哘雅楽乃 <yusk5497hnd@gmail.com>
2018-03-19 18:44:47 +0900

    Add files via upload

---
+++ modules/user/build/CMakeFiles/user.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/user.dir/main.c.obj"
+  "user.pdb"
+  "user"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/user.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
